3f5933ba7be6635cd81f972c16f11b66fe140517
Dipesh Gandhi <dipesh.gandhi@humanify.com>
2017-05-09 13:38:03 -0500

    added sort, promise

--- middleware/airlinepartner.js
+++ middleware/airlinepartner.js
@@ -5,22 +5,76 @@ const client = new Client();
 var Promise = require('promise');
 const SEARCH_URL = process.env.SEARCH_URL || 'http://localhost:9000/scrapers/';
 
+//This could be comma delimted env variable, or pulled from config
+var partners = ['Expedia', 'Orbitz', 'Priceline', 'Travelocity', 'United'];
+
 module.exports = class AirlinePartner {
 
    constructor() {
-
+      this.self = this;
    }
 
    search(provider) {
-     console.log('--> search invoked ', provider);
+     console.log('--> search invoked for', provider);
      return new Promise(function (fulfill, reject){
          client.get(SEARCH_URL + provider , function (data, response) {
-              console.log('success');
-              fulfill(data);
+              console.log('--> search success for', provider);
+              fulfill(data.results);
           }).on('error', function (err) {
-              console.log('error');
+              console.log('--> search error for', provider);
               reject(err);
           });
      });
+   }
+
+   searchAll(){
+
+     var self = this;
+
+     console.log('--> search all invoked');
+     return new Promise(function (fulfill, reject){
+
+           //Create search task for each partner, later executed by Promise.all
+           var tasks = [];
+           for(var i in partners){
+                tasks.push(self.search(partners[i]));
+           }
+
+           //Do actual search for all defined profiders async, then sort results by agony
+           Promise.all(tasks).then( values => {
+
+              //Format to structure we can sort
+              var result = [];
+              for(var key in values){
+                if (values.hasOwnProperty(key)){
+                  for(var entry in values[key]){
+                    if (values[key].hasOwnProperty(entry)){
+                        result.push(values[key][entry]);
+                    }
+                  }
+                }
+              }
+
+              //Sort result set
+              result.sort((a,b) => {
+                if(a.agony > b.agony ){
+                  return 1;
+                }else if (a.agony < b.agony ){
+                  return -1;
+                }else{
+                  return 0;
+                }
+              });
+
+              //support 'results' json structure
+              var response = new Object();
+              response.results = result;
+
+              fulfill(response);
+
+           }).catch( err => {
+              reject(err);
+           });
+      });
    }
 }
--- routes/flights.js
+++ routes/flights.js
@@ -4,7 +4,7 @@ var express = require('express')
   , router = express.Router();
 var Promise = require('promise');
 var AirlinePartner = require('../middleware/airlinepartner.js');
-var partners = ['Expedia', 'Orbitz', 'Priceline', 'Travelocity', 'United'];
+
 
 router.get('/', function(req, res) {
      res.send('This is flights endpoint');
@@ -16,19 +16,19 @@ router.get('/search', function(req, res) {
 
      console.log('Request for Flight Search');
 
-     //Get Partner object
-     var partner = new AirlinePartner();
-
-     //Create search tasks for each partner
-     var tasks = [];
-     for(var i in partners){
-          tasks.push(partner.search(partners[i]));
-     }
-
-     //Do actual search for provides async
-     Promise.all(tasks).then( values => {
-        return res.send(JSON.stringify(values));
+     //Search All Airline Partners
+     var airlinePartner = new AirlinePartner();
+     airlinePartner.searchAll().then( data => {
+          return res.status(200).json(data);
+     }).catch(err => {
+       return res.status(400).json({
+           status: {
+               code: 400,
+               errorType: err.message
+           }
+       });
      });
+
 
 
 })
